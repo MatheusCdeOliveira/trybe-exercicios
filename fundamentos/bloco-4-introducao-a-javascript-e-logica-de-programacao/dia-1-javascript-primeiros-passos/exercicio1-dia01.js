f1eebb5aa4c13cad909f2f074cd6c2a6bc6e1a82
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-06-21 19:03:14 -0300

    primeira etapa do exercicio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1-dia01.js
@@ -0,0 +1,12 @@
+// Faça cinco programas, um para cada operação aritmética básica.
+//  Seu programa deve ter duas constantes, a e b, 
+//  definidas no começo com os valores que serão operados. 
+
+const a = 38;
+const b = 8;
+
+console.log("soma =", +a + b);
+console.log("subtração =", +a - b);
+console.log("multiplicação =", +a * b);
+console.log("divisão =", +a / b);
+console.log("resto =", +a % b);
