e926ee8c812b80a939c97cd08ee2761c8a8860ee
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-06-21 22:11:54 -0300

    exercicio 8 e 9 resolvidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1-dia01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1-dia01.js
@@ -136,3 +136,33 @@ if (nota > 100 || nota < 0) {
 } else {
   console.log("F");
 }
+
+
+// 8. Escreva um programa que defina três números em constantes e 
+//  retorne true se pelo menos uma das três for par. 
+//  Caso contrário, ele retorna false.
+
+const n1 = 1;
+const n2 = 5;
+const n3 = 3;
+
+ if (n1 % 2 === 0|| n2 % 2 === 0 || n3 % 2 === 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+
+
+
+const n1 = 2;
+const n2 = 4;
+const n3 = 7;
+
+ if (n1 % 2 !== 0|| n2 % 2 !== 0 || n3 % 2 !== 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+
