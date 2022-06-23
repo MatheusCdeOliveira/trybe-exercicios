91e36b3aca5f3e64d846e4fb05450cb0a9d25cb4
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-06-23 20:58:37 -0300

    Exercicios 2 e 3 resolvidos.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritimos/exercicios-js.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritimos/exercicios-js.js
@@ -1,4 +1,4 @@
-// Exercicio 1 - retorno o fatorial de 10 
+// Exercicio 1 - retorne o fatorial de 10 
 
 let fatoracao = 1;
 for (let index = 10; index > 0; index -= 1) {
@@ -6,4 +6,41 @@ for (let index = 10; index > 0; index -= 1) {
 
 }
 console.log(fatoracao)
+
+
+// Exercicio 2. inverter a palavra.
+
+
+let word = 'tryber';
+let reverseWord = '';
+
+for (index = 0; index < word.length; index += 1){
+    reverseWord += word[word.length - 1 - index]
+}
+console.log(reverseWord)
+
+
+// Exercicio 3. Retornar a maior e a menor palavra do array
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+ for (index = 0; index < array.length; index += 1){
+    if(array[index].length > maiorPalavra.length) {
+        maiorPalavra = array[index]
+        console.log('Maior palavra do array:', maiorPalavra)
+    } 
+}
+
+for (index2 = 0; index2 < array.length; index2 += 1) {
+    if(array[index2].length < menorPalavra.length) {
+        menorPalavra = array[index2]
+        console.log('Menor palavra do array:', menorPalavra)
+    }
+}
+
+
+
+
 
